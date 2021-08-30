1dbda300a7edca8ddd1fa4cacbf88bbac1ff19cc
khadija-6 <khadija6.abdi@protonmail.com>
2021-08-30 18:12:52 +0300

    first commit

---
+++ fuction.py
@@ -0,0 +1,45 @@
+# Function
+# Day1
+
+# Morning routine function
+def morning_routine():
+    print("Wake up")
+    print("Go to the washroom")
+    print("Brush your teeth")
+    print("Pray")
+    print("Wear uniform")
+    print("Eat breakfast")
+    print("take your bag and go to school!")
+
+# Day 1
+print("")
+morning_routine()
+
+# Day 2
+print("")
+morning_routine()
+#print("Wake up")
+#print("Go to the washroom")
+#print("Brush your teeth")
+#print("Pray")
+#print("Wear uniform")
+#print("Eat breakfast")
+#print("take your bag and go to school!")
+
+# Day 3
+#print("Wake up")
+#print("Go to the washroom")
+#print("Brush your teeth")
+#print("Pray")
+#print("Wear uniform")
+#print("Eat breakfast")
+#print("take your bag and go to school!")
+
+# Day 4
+#print("Wake up")
+#print("Go to the washroom")
+#print("Brush your teeth")
+#print("Pray")
+#print("Wear uniform")
+#print("Eat breakfast")
+#print("take your bag and go to school!")
